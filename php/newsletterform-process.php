ff9bafb6e9f1e740ffccf477e8300e806affc058
ankushzambre <zambreankush@gmail.com>
2022-05-07 10:06:17 +0530

    AZ-Initial set up

---
+++ php/newsletterform-process.php
@@ -0,0 +1,38 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "yourname@domain.com";
+$Subject = "New newsletter subscription from Corso landing page";
+
+$Body = "";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
